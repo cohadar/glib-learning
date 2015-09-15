c140f05c495d84de8970fa60aa32547da39a5d07
Damir Cohadarevic <damir.cohadarevic@sbgenomics.com>
2015-09-15 19:09:41 +0200

    g_slice

--- main.c
+++ main.c
@@ -2,11 +2,24 @@
 #include <glib.h>
 #include "../asrt/asrt.h"
 
+void test_g_slice()
+{
+	struct Njak{
+		int x, y;
+	};
+	struct Njak *njak =g_slice_new(struct Njak);
+	njak->x = 55;
+	njak->y = 777;
+	struct Njak *njak2 = g_slice_dup(struct Njak, njak);
+	ASRT_int(55, njak2->x);
+	ASRT_int(777, njak2->y);
+	g_slice_free(struct Njak, njak);
+	g_slice_free(struct Njak, njak2);
+}
 
 int
 main(int argc, char** argv) {
-	GList* list = NULL;
-	list = g_list_append(list, "Hello world!");
-	printf("The first item is '%s'\n", g_list_first(list)->data);
+	test_g_slice();
+	printf("All Ok!\n");
 	return 0;
 }
